7f653850b288ca8fb7c875a8d2c9e852ffd8ff35
Maycon-crz <mayconnascimentodeoliveira23@gmail.com>
2022-05-03 02:51:59 -0300

    aula sobre receber parametro pela url

--- curso_backend_deivid_willyan/bin/curso_backend_deivid_willyan.dart
+++ curso_backend_deivid_willyan/bin/curso_backend_deivid_willyan.dart
@@ -1,10 +1,9 @@
-import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as shelf_io;
 
 import 'server_handler.dart';
 
 void main() async {
-  var _server = ServerH();
+  var _server = ServerHandler();
 
   final server = await shelf_io.serve(_server.handler, 'localhost', 8081);
 
--- curso_backend_deivid_willyan/bin/server_handler.dart
+++ curso_backend_deivid_willyan/bin/server_handler.dart
@@ -1,7 +1,7 @@
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
 
-class ServerH {
+class ServerHandler {
   Handler get handler {
     final router = Router();
 
@@ -9,6 +9,25 @@ class ServerH {
       return Response(200, body: 'Primeira Rota');
     });
 
+    //http://localhost:8081/ola/mundo
+    //Ola mundo
+    router.get('/ola/mundo/<usuario>', (Request req, String usuario) {
+      return Response.ok("Ola mundo $usuario");
+    });
+
+    //Obs: Depois que criei esse parou de funcionar mas estava funcionando tudo inclusive esse então
+    //Se der erro provavelmente deve ser pouca coisa
+    //-----------------------
+    //Este próximo método não é muito seguro
+    //http://localhost:8081/ola/mundo?nome=Maycon&idade=23
+    // router.get('/query', (Request req) {
+    //   String? nome = req.url.queryParameters['nome'];
+    //   String? idade = req.url.queryParameters['idade'];
+    //   return Response.ok('Query eh: $nome, idade $idade');
+    // });
+
     return router;
   }
 }
+
+////////PAREI NO INICIO DA AULA 06
